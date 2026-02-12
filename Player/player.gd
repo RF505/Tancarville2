a58d78d26be7bb3c264931b09b8845fcd8f66c90
RF505 <rf505yt@gmail.com>
2026-02-12 20:36:23 +0100

    Anims

--- Player/player.gd
+++ Player/player.gd
@@ -1,34 +1,43 @@
 extends CharacterBody2D
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var anim = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const MAX_STEP_HEIGHT = 16
 
 func _physics_process(delta: float) -> void:
-	# Gravity
+	# --- Gravité ---
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-	# Jump
+		velocity.y += get_gravity().y * delta
+
+	# --- Saut ---
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	# --- Mouvement horizontal ---
 	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	# 🔹 On sauvegarde la position avant mouvement
-	var previous_position = position
-	# A compléter 
+	velocity.x = move_toward(velocity.x, direction * SPEED, SPEED)
+
+	# --- Animations ---
 	if Input.is_action_just_pressed("sing"):
-		animation_player.play("run")
+		anim.play("sing")
+	elif direction != 0:
+		if anim.current_animation != "run":
+			anim.play("run")
+		sprite.flip_h = direction < 0
+	else:
+		if anim.current_animation != "sing" and anim.current_animation != "idle":
+			anim.play("idle")
+
+	# --- Déplacement ---
+	var previous_position = position
 	move_and_slide()
-	# 🔹 Si on est bloqué horizontalement
+
+	# --- Gestion des marches ---
 	if is_on_floor() and direction != 0 and is_on_wall():
-		# On revient à la position précédente
 		position = previous_position
-		# On tente de monter progressivement
 		for i in range(MAX_STEP_HEIGHT):
 			position.y -= 1
 			if not test_move(global_transform, Vector2(direction * 1, 0)):
