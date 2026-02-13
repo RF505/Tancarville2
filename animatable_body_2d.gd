ec6dfb8afafca2dc1b4a41044b5ea711512c6db6
martykerst-creator <martykerst@gmail.com>
2026-02-13 04:51:05 +0100

    ASCENSCEUR FINI PTN

--- animatable_body_2d.gd
+++ animatable_body_2d.gd
@@ -1,56 +1,50 @@
 extends AnimatableBody2D
 
-# --- CONFIGURATION ---
-@export var vitesse = 150.0
-@export var temps_pause = 5.0
-# Glisse ton Line2D dans cette case dans l'inspecteur
+@export var vitesse : float = 150.0
+@export var temps_pause : float = 5.0
 @export var corde_node : Line2D 
+@export var attache_plafond : Node2D 
 
-# --- NODES ---
 @onready var ray_sol = $RayCast2Dsol
 @onready var timer = $WaitTimer
 
-# --- VARIABLES INTERNES ---
 var pos_haut : Vector2
 var vers_le_bas = true
 var en_pause = false
 
 func _ready():
-	# On enregistre la position de départ (le haut)
-	pos_haut = global_position
-	
-	# Configuration du Timer
-	timer.wait_time = temps_pause
-	timer.one_shot = true
-	timer.timeout.connect(_on_timer_timeout)
-	
-	# Initialisation de la corde
-	if corde_node:
-		# Le point 0 est l'attache fixe (en haut)
-		# On suppose que le Line2D est placé au point d'attache
-		corde_node.set_point_position(0, Vector2.ZERO)
-
-func _physics_process(delta):
-	# 1. MISE À JOUR DE LA CORDE
-	if corde_node:
-		# On force le point 1 de la ligne à suivre la plateforme
-		# to_local convertit la position globale de la plateforme en coordonnée pour la ligne
-		var pos_relative = corde_node.to_local(global_position)
-		corde_node.set_point_position(1, pos_relative)
-
-	# 2. GESTION DE LA PAUSE
-	if en_pause:
+	# On s'assure que tout est bien configuré
+	if not timer or not corde_node or not attache_plafond:
+		print("!!! ATTENTION : Il manque des Nodes dans l'inspecteur !!!")
 		return
 
-	# 3. LOGIQUE DE MOUVEMENT
+	pos_haut = global_position
+	timer.wait_time = temps_pause
+	timer.one_shot = true
+	if not timer.timeout.is_connected(_on_timer_timeout):
+		timer.timeout.connect(_on_timer_timeout)
+
+	# Initialisation corde
+	while corde_node.get_point_count() < 2:
+		corde_node.add_point(Vector2.ZERO)
+
+func _physics_process(delta):
+	# --- CORDE ---
+	if corde_node and attache_plafond:
+		# Le point 0 suit l'attache au plafond
+		corde_node.set_point_position(0, corde_node.to_local(attache_plafond.global_position))
+		# Le point 1 suit la plateforme (global_position est le centre de ton node)
+		corde_node.set_point_position(1, corde_node.to_local(global_position))
+
+	# --- MOUVEMENT ---
+	if en_pause: return
+
 	if vers_le_bas:
-		# On descend jusqu'à toucher la TileMap
-		if ray_sol.is_colliding():
+		if ray_sol and ray_sol.is_colliding():
 			lancer_pause()
 		else:
 			global_position.y += vitesse * delta
 	else:
-		# On remonte jusqu'au point de départ
 		if global_position.y <= pos_haut.y:
 			global_position.y = pos_haut.y
 			lancer_pause()
@@ -62,6 +56,5 @@ func lancer_pause():
 	timer.start()
 
 func _on_timer_timeout():
-	# Inversion de la direction après les 5 secondes
 	vers_le_bas = !vers_le_bas
 	en_pause = false
